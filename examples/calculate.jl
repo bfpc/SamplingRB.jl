d1f43f5dea537f92900778131d9440af37baa99d
Bernardo Freitas Paulo da Costa <bernardofpc@im.ufrj.br>
2022-01-12 16:15:05 -0300

    Change from RData to NPZ in example

--- examples/calculate.jl
+++ examples/calculate.jl
@@ -1,16 +1,14 @@
 import Pkg
 Pkg.activate(".")
 
-import RData
 import NPZ
-using Printf: @sprintf
 using CvarRiskParity
 
 
 # Parameters
-dir  = joinpath("..", "data", "processed")
-pricesf = joinpath(dir, "allPrices.RData")
-simulsf = d -> joinpath(dir, "$(d)_prices_sim.RData")
+dir  = "data"
+pricesf = joinpath(dir, "allPrices.npz")
+simulsf = d -> joinpath(dir, "$(d)_prices_sim.npz")
 outf = joinpath("results", "095", "weights.npz")
 
 B = ones(11)
@@ -19,14 +17,14 @@ alpha = 0.95
 days = [1, 5, 485]
 
 # Script itself
-true_prices = RData.load(pricesf)["allPrices"]
+true_prices = NPZ.npzread(pricesf)["allPrices"]
 
 ws = Vector{Float64}[]
 hards = Int64[]
 
 for d in days
     print(d, ", ")
-    prices_sim   = RData.load(simulsf(d))["prices_sim"][1,1:50,:]
+    prices_sim   = NPZ.npzread(simulsf(d))["prices_sim"]
     prices_today = true_prices[d,:]
     relative_losses = 1 .- prices_sim' ./ prices_today
     status, w = cvar_rbp(B, alpha, relative_losses)
@@ -36,13 +34,17 @@ for d in days
     end
     push!(ws, w)
 end
+println("done!")
+println()
 
-println("Scenarios with too large weights, maybe not optimal since other active constraints:")
+println("Scenarios with too large weights:")
+println("  (Likely the corresponding risk parity problem is unbounded below)")
 for i in 1:length(ws)
     if any(ws[i] .> 1000)
         println("day ", days[i], " ", ws[i])
     end
 end
 
+# Saving weights for later use
 mkpath(dirname(outf))
 NPZ.npzwrite(outf, Dict("ws" => hcat(ws...)))

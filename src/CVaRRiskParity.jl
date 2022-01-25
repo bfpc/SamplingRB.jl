a7df70144b5d59cbae513a68173f6d96750298b2
Bernardo Freitas Paulo da Costa <bernardofpc@im.ufrj.br>
2022-01-25 15:55:24 -0300

    Detail format for the matrix of simulations

--- src/CVaRRiskParity.jl
+++ src/CVaRRiskParity.jl
@@ -39,6 +39,8 @@ include("cvar_cp.jl") # Cutting Plane algorithm, and related utilities
 Compute the investment weights on the assets in order to build a
 CV@R-`alpha` risk budgeting portfolio given risk appetites in `B`
 and a matrix of relative losses in `rel_losses`.
+This matrix has size  (|B|, nsamples), so that each column corresponds
+to a sample of relative losses, and each row to a given asset.
 
 The algorithm stops after reaching a provable optimality gap within
 (both absolute and relative) tolerance `tol`, or after `maxiters` iterations,
